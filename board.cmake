8d1ebd62a22dc31e9256026001fb75f30661674a
alexmoshte <alexmoshte@gmail.com>
2026-02-06 11:14:14 +0300

    uf2 added

--- board.cmake
+++ board.cmake
@@ -1,5 +1,11 @@
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
 board_runner_args(jlink "--device=nRF52840_xxAA" "--speed=4000")
+
+set(BOARD_UF2_FAMILY_ID 0xADA52840)
+
+include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 
 include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
